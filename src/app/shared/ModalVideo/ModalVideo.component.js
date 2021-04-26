2457807eb769779feafffc81a6118ee31515a2d5
Arangojd26 <arangojd26@gmail.com>
2021-04-26 15:41:35 -0500

    modal corregido con typed en el mensaje y autoplay del video

--- src/app/shared/ModalVideo/ModalVideo.component.js
+++ src/app/shared/ModalVideo/ModalVideo.component.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Modal, ModalBody, ModalFooter } from "reactstrap";
+import { Modal, ModalBody } from "reactstrap";
 import "./ModalVideo.css";
 import ReactPlayer from "react-player";
-// import YouTube from "react-youtube";
+import Typed from "react-typed";
 import GenericButton from "../GenericButton/GenericButton.component";
 
 const ModalVideo = (props) => {
   const [modal, setModal] = React.useState(false);
-
+  const textModal = "Texto de prueba para generar una frase motivadora.";
   const toggle = () => setModal(!modal);
 
   return (
@@ -16,22 +16,33 @@ const ModalVideo = (props) => {
       <Modal
         isOpen={modal}
         toggle={toggle}
-        className="modal-dialog modal-dialog-centered modal-lg modal-video"
+        className="modal-dialog-centered modal-lg"
       >
-        <ModalBody className="m-0 p-0">
-          <ReactPlayer
-            url={`https://www.youtube.com/watch?v=${props.url}`}
-            width="100%"
-            height="28rem"
-            controls={true}
-          />
-        </ModalBody>
-        <ModalFooter className="o-modal-footer">
+        <div className="modal-content">
+          <ModalBody className="o-modal-body">
+            <div className="o-container-video-modal">
+              <ReactPlayer
+                url={`https://www.youtube.com/watch?v=${props.url}`}
+                width="100%"
+                height="100%"
+                controls={true}
+                playing={true}
+              />
+            </div>
+
+            <div className="d-flex justify-content-center mt-4">
+              <div className="o-container-text-modal text-light">
+                <div className="o-text-modal">
+                  <Typed strings={[textModal]} typeSpeed={65} />
+                </div>
+              </div>
+            </div>
+          </ModalBody>
           <GenericButton title={"Cerrar"} clickButton={toggle} />
-        </ModalFooter>
+        </div>
       </Modal>
     </>
   );
 };
 
-export default ModalVideo;
+export default React.memo(ModalVideo);
